7660975f19385a7140b5c7bb3fc41e6bc73f2df2
AkiraFukushima <h3.poteto@gmail.com>
2019-10-07 21:54:46 +0900

    Initial commit

---
+++ lib/ueberauth_hatena.ex
@@ -0,0 +1,18 @@
+defmodule UeberauthHatena do
+  @moduledoc """
+  Documentation for UeberauthHatena.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UeberauthHatena.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ueberauth_hatena_test.exs
@@ -0,0 +1,8 @@
+defmodule UeberauthHatenaTest do
+  use ExUnit.Case
+  doctest UeberauthHatena
+
+  test "greets the world" do
+    assert UeberauthHatena.hello() == :world
+  end
+end
