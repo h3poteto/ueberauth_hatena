afd9330589e9692911d3c51e667bc0bbd2c54cc4
AkiraFukushima <h3.poteto@gmail.com>
2019-10-08 00:38:37 +0900

    Add credo

--- mix.exs
+++ mix.exs
@@ -7,7 +7,13 @@ defmodule UeberauthHatena.MixProject do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "Hatena strategy  for Ueberauth.",
+      package: [
+        maintainers: ["h3poteto"],
+        licenses: ["MIT"],
+        links: %{"GitHub" => "https://github.com/h3poteto/ueberauth_hatena"}
+      ]
     ]
   end
 
@@ -23,9 +29,8 @@ defmodule UeberauthHatena.MixProject do
     [
       {:httpoison, "~> 1.6"},
       {:oauther, "~> 1.1"},
-      {:ueberauth, "~> 0.6"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ueberauth, "~> 0.6"},
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false}
     ]
   end
 end
