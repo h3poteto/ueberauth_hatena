72bf28a6de08d0d771748f9ac42b33c4db6e202a
AkiraFukushima <h3.poteto@gmail.com>
2019-10-07 23:06:35 +0900

    Downgrade elixir version to 1.5

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule UeberauthHatena.MixProject do
     [
       app: :ueberauth_hatena,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
