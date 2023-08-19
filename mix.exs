d29c4f1dd247fa94c2d937f7a2702631da4149d1
AkiraFukushima <h3.poteto@gmail.com>
2023-08-19 22:57:14 +0900

    Bump to version 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule UeberauthHatena.MixProject do
   def project do
     [
       app: :ueberauth_hatena,
-      version: "0.1.0",
-      elixir: "~> 1.5",
+      version: "0.2.0",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: "Hatena strategy  for Ueberauth.",
